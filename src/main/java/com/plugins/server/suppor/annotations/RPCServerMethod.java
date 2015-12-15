7c3b44f8832defd7aaebc3be4ce73f363ed47fde
javazhangtao <javazhangtao@gmail.com>
2015-12-15 19:51:15 +0800

    修改4

--- src/main/java/com/plugins/server/suppor/annotations/RPCServerMethod.java
+++ src/main/java/com/plugins/server/suppor/annotations/RPCServerMethod.java
@@ -9,6 +9,7 @@ import java.lang.annotation.*;
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
+@Deprecated
 public @interface RPCServerMethod {
     String version();//版本号
 }
