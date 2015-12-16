540a3b775036e83ee8bbfabfa3e7b4ee5c7db548
javazhangtao <javazhangtao@gmail.com>
2015-12-16 14:06:18 +0800

    修改4

--- src/main/java/com/plugins/server/suppor/resource/ServerResource.java
+++ src/main/java/com/plugins/server/suppor/resource/ServerResource.java
@@ -11,7 +11,7 @@ import org.apache.commons.pool2.impl.GenericKeyedObjectPool;
  */
 public class ServerResource {
 
-    public static final PropertiesUtil responsecode_prop=new PropertiesUtil("/properties/response_code.properties");
+    public static final PropertiesUtil responsecode_prop=new PropertiesUtil("/properties/response_code.properties");//服务响应状态码配置信息对象
     public static GenericKeyedObjectPool<String,FastClass> serverPool=null;//服务池集合
     public static ServerPoolConfig poolConfig=new ServerPoolConfig();//代理类池配置
 
